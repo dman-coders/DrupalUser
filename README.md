ba7f88eea8da21ec48997d69261a006aa7befc55
Dan (dman) Morrison <dan@coders.co.nz>
2016-08-05 01:37:18 +1200

    Added logger. Start supporting input options.

--- README.md
+++ README.md
@@ -6,3 +6,11 @@ Built with parts inspired by or scrounged from
 (Drush Issue Queue Commands)[https://www.drupal.org/project/drush_iq]
 and the Drupal Test Suite harness.
 
+To avoid having to enter credentials or connection details
+all the time, it's recommended to place the commonly needed options
+in your drushrc.php file like so
+
+    # du Options
+    $command_specific['du-login']['user'] = 'AzureDiamond';
+    $command_specific['du-login']['pass'] = 'hunter2';
+     
--- src/du/DrupalUser.php
+++ src/du/DrupalUser.php
@@ -3,7 +3,7 @@
 namespace Drupal\du;
 
 use Goutte\Client;
-
+use Psr\Log\LoggerInterface;
 
 /**
  * Drupal User Agent, for interacting with Drupal style websites.
@@ -14,9 +14,18 @@ class DrupalUser extends Client {
 
   private $site = array(
     'url' => 'https://www.drupal.org',
-    'username' => 'AzureDiamond',
-    'password' => 'hunter2',
+    'username' => '',
+    'password' => '',
   );
+
+  private $authenticated = NULL;
+
+  /**
+   * The logger channel.
+   *
+   * @var \Psr\Log\LoggerInterface
+   */
+  protected $logger;
 
   /**
    * A DrupalUser Client.
@@ -31,7 +40,7 @@ class DrupalUser extends Client {
    */
   public function __construct($site_def = array()) {
     // TODO validation.
-    $this->site = $site_def + $this->site;
+    $this->site = (array)$site_def + $this->site;
 
     parent::__construct();
   }
@@ -58,6 +67,9 @@ class DrupalUser extends Client {
    */
   public function getSiteInfo() {
     $info = array();
+
+    $this->log("Contacting server...", array('!url' => $this->site['url']));
+
     /** @var \Symfony\Component\DomCrawler\Crawler $crawler */
     $crawler = $this->request('GET', $this->site['url']);
     $info['HTML Title'] = $crawler->filter('title')->text();
@@ -79,17 +91,57 @@ class DrupalUser extends Client {
       $info['Server'] = $response->getHeader('Server');
     }
 
+    if (!empty($this->site['username'])) {
+      // It's worth trying to log in.
+      $this->login();
+      if (isset($this->authenticated)) {
+        $info['Authenticated'] = $this->authenticated;
+      }
+    }
+
     return $this->site + $info;
   }
 
   public function login() {
+    $this->log("Authenticating...", array());
     $login_url = $this->site['url'] . '/user';
     $crawler = $this->request('GET', $login_url);
     $form = $crawler->selectButton('Log in')->form();
     // Beware TFA here now!
-    $crawler = $this->submit($form, array('name' => $user, 'pass' => $pass));
 
-    return $this->site;
+    $crawler = $this->submit($form, array('name' => $this->site['username'], 'pass' => $this->site['password']));
+
+    // See if that looked like a success
+    $this->authenticated = 'dunno';
+    return $this;
+  }
+
+
+  /**
+   * Log a message.
+   *
+   * Using a Symfony Logger interface if one was provided,
+   * or just the drush_log if not.
+   *
+   * @param $message
+   * @param $strings
+   * @param $level
+   */
+  private function log($message, $strings, $logLevel = 'notice') {
+    if (is_callable('xdebug_call_function')) {
+      $function = xdebug_call_function();
+    }
+    else {
+      $function = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 2)[1]['function'];
+    }
+
+    if ($this->logger) {
+      /* \Psr\Log\LoggerInterface $logger */
+      $this->logger->log($logLevel, '[' . $function . '] ' . dt($message, $strings));
+    }
+    else {
+      drush_log('[' . $function . '] ' . dt($message, $strings), $logLevel);
+    }
   }
 
 }
