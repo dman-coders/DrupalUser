71ecfad4cef554d3e8385da63647ae01189d7f09
Dan (dman) Morrison <dan@coders.co.nz>
2016-08-04 22:53:47 +1200

    Set up the hooks for a drush command that uses OO to create my Client class.

---
+++ README.md
@@ -0,0 +1,8 @@
+# Drush Drupal User
+
+A User agent for authenticating and interacting with a Drupal Site.
+
+Built with parts inspired by or scrounged from 
+(Drush Issue Queue Commands)[https://www.drupal.org/project/drush_iq]
+and the Drupal Test Suite harness.
+
---
+++ src/du/DrupalUser.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Drupal\du;
+
+use Goutte\Client;
+
+
+/**
+ * Drupal User Agent, for interacting with Drupal style websites.
+ *
+ * \Drupal\du\DrupalUser.
+ */
+class DrupalUser extends Client {
+
+  private $site = array(
+    'url' => 'https://www.drupal.org',
+    'username' => 'AzureDiamond',
+    'password' => 'hunter2',
+  );
+
+  /**
+   * A DrupalUser Client.
+   *
+   * @param array $site_def
+   *   A keyed array of connection details for connecting to a site.
+   *   Expected keys:
+   *   - url
+   *   - username
+   *   - password
+   *   .
+   */
+  public function __construct($site_def = array()) {
+    // TODO validation.
+    $this->site = $site_def;
+  }
+
+  /**
+   * Fetch attributes about a site.
+   *
+   * @param string $attr
+   *   Site attribute to fetch.
+   *
+   * @return string|array
+   */
+  public function getSite($attr = NULL) {
+    if (!empty($attr) && isset($this->site[$attr])) {
+      return $this->site[$attr];
+    }
+    return $this->site;
+  }
+
+  /**
+   * Fetch info from the site.
+   *
+   * Makes a connection and scrapes some basic diagnostics.
+   */
+  public function getSiteInfo() {
+    return $this->site;
+  }
+
+}
